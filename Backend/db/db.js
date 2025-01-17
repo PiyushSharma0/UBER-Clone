1457486a0a2d0e609bfeb417956849ad4c217090
PiyushSharma0 <piyush.sharma.code@gmail.com>
2025-01-17 16:09:36 +0530

    User Authentication Done

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 function connectToDb() {
     mongoose.connect(process.env.DB_CONNECTION).then(() => {
         console.log("Connected to DB");
-    }).catch(err => console.log(err));
+    }).catch(error => console.log(error));
 }
 
 module.exports = connectToDb;
